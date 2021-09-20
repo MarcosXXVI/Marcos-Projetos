379547258b0a99b4179f7dfee8542d6ce58ae5f8
MarcosXXVI <85961735+MarcosXXVI@users.noreply.github.com>
2021-09-20 16:43:45 -0300

    NetflixClone-SemCarrossel
    Precisei retirar o carrossel de filmes do projeto, pois com o tamanho do arquivo eu não consegui colocar no Github.

---
+++ Netflix - Sem Carrossel/js/main.js
@@ -0,0 +1,18 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:8,
+    nav:false,
+    responsive:{
+        0:{
+            items:3
+        },
+        600:{
+            items:4
+        },
+        1000:{
+            items:8
+        }
+    }
+})
+
+
