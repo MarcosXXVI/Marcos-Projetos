f3b882574b03787eecc06be2fab0d4cd8bbf54e7
MarcosXXVI <85961735+MarcosXXVI@users.noreply.github.com>
2021-09-20 15:59:30 -0300

    Projeto-Serieflix
    Foi preciso reduzir o projeto pois o Github não estava aceitando a quantidade de pastas do projeto original.

---
+++ Netflix Clone - Nova Roupagem - 2/js/serie.js
@@ -0,0 +1,17 @@
+/* Usando mouseover e mouseout */
+
+function trocarImagem(){
+    document.getElementById("img1").src="/img/Séries/contraCapa.jpg";
+}
+
+function retornarImagem(){
+    document.getElementById("img1").src="/img/Séries/mini12.jpg";
+}
+
+function trocarImagem2(){
+    document.getElementById("img2").src="/img/Séries/contraCapa2.jpg";
+}
+
+function retornarImagem2(){
+    document.getElementById("img2").src="/img/Séries/mini13.jpg";
+}
